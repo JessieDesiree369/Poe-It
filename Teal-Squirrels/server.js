a26028085f0838a2b96aa55c82416fd0a96ce850
dccoppock@gmail.com <dccoppock@gmail.com>
2022-09-08 17:58:02 -0400

    Added main.handlebars and test html file

--- Teal-Squirrels/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3006;
-
-app.get("/", async (req, res) => {
-    res.send("Werking.");
-})
-
-app.listen(PORT, () => console.log("doing the thing fr fr"))
