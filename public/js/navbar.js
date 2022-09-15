fa9c09cfbc6bc974651a491d4bfea5a702aff371
dccoppock@gmail.com <dccoppock@gmail.com>
2022-09-15 13:08:10 -0400

    added basic animation

---
+++ public/js/navbar.js
@@ -0,0 +1,60 @@
+//const anime = require("animejs");
+
+const leftNav = document.querySelectorAll(".leftnav");
+const rightNav = document.querySelectorAll(".rightnav");
+const mainBody = document.querySelectorAll(".main-body");
+const titleBar = document.querySelectorAll(".title-bar");
+
+// --------- moveDuration sets how long all animations take in milliseconds
+const moveDuration = 500;
+
+const mainLoadTL = anime.timeline({
+  easing: "easeOutSine",
+  duration: moveDuration,
+  direction: "reverse"
+});
+
+mainLoadTL.add({
+  targets: rightNav,
+  translateX: 200,
+  //direction: "reverse",
+  easing: "easeInSine"
+});
+
+mainLoadTL.add({
+  targets: leftNav,
+  translateX: -200,
+  //direction: "reverse",
+  easing: "easeInSine"
+});
+
+mainLoadTL.add({
+  targets: titleBar,
+  translateY: -160,
+  //direction: "reverse",
+  easing: "easeInSine"
+});
+
+anime({
+  targets: mainBody,
+  translateY: 300,
+  direction: "reverse",
+  duration: moveDuration/2,
+  opacity: 0,
+  easing: "easeInSine"
+});
+
+anime({
+  targets: ".path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  delay: function(el, i) {
+    return i * 250;
+  },
+  direction: "alternate",
+  loop: true
+});
+
+// document
+//   .querySelector(".login-form")
+//   .addEventListener("submit", loginFormHandler);
